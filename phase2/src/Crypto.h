616144b9faea668d3c45114f468e027b6015ec94
ZZWH123 <mengqi2025_1212@163.com>
2025-10-21 15:17:23 +0800

    Update Crypto.h

--- phase2/src/Crypto.h
+++ phase2/src/Crypto.h
@@ -3,7 +3,7 @@
 #define CRYPTO_H         //定义宏
 #include <string>
 class Crypto{            //定义Crypto这个类 
-public：                 //用XOR加密解密 
+public:                 //用XOR加密解密 
     static std::string XORCipher(const std::string& input,char key);
     };
 #endif     
