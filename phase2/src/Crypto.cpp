596e6c740226075335e7899a9d6fe8ff0b0c73ec
ZZWH123 <mengqi2025_1212@163.com>
2025-10-21 15:07:59 +0800

    Update Crypto.cpp

--- phase2/src/Crypto.cpp
+++ phase2/src/Crypto.cpp
@@ -1,7 +1,7 @@
 #include "Crypto.h"
 std::string Crypto::XORCipher(const std::string&input,char key){
 	std::string output =input; //创建与输入相同大小的字符串 
-	for(size_t i=0;i<inpt.length();++i){
+	for(size_t i=0;i<input.length();++i){
 		output[i]=input[i]^key;//每个字符都与同一个密钥数字进行XOR 
 	}
 	return output;
